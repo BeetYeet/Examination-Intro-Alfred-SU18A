cadc922823a932a8b1e7da01f282d5b36fd9c872
Alfred Desktop <alfred.engstromer@elev.ga.lbs.se>
2018-10-08 06:07:54 +0200

    Day 4
    Massive improvements to world script
    Inplemented Pausable as deribable class

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class World : MonoBehaviour {
+public class World: MonoBehaviour
+{
 
 	public static World current //the one and only instance of world
 	{
 		get;
 		private set;
-	}	
+	}
 	public bool isPaused
 	{
 		get
@@ -16,21 +17,26 @@ public class World : MonoBehaviour {
 			return pauseCodes.Count == 0;
 		}
 	}
-	public event System.Action OnPauseChange;
+	public event System.Action OnPauseChange; // all things that should be pausable must subscribe to this
+	public event System.Action OnGameStart;
 
 	/*
 	 * Special pause codes:
 	 * 0 - requires force to be unpaused
+	 * 1 - startup
 	 */
 	private List<uint> pauseCodes; // if a pausecode appears here, dont unpause untill its gone
 
-	void Awake () { // make it awake so that it is called way before all other scripts so that they can cashe it in Start()
+	void Awake()
+	{ // make it awake so that it is called way before all other scripts so that they can cashe it in Start()
 		if ( World.current != null ) // íf there already is an instance of World
 		{
 			Debug.LogError( "Multiple instances of World exist, please keep it to one", this ); // throw an error
 			return; // and return
 		}
 		World.current = this; // else just set it to this instance
+		pauseCodes = new List<uint>();
+		Pause( 1 ); // pause the game and wait for input so that we know that the user is ready to play
 	}
 
 
@@ -39,11 +45,11 @@ public class World : MonoBehaviour {
 	/// </summary>
 	/// <param name="pauseCode">a unique code for the specific purpouse of the pause so that you can pause from multiple sources (for example opening the pause menu during an event)</param>
 	/// <returns>returns true if it wasn't paused before</returns>
-	public bool Pause( uint pauseCode ) 
+	public bool Pause( uint pauseCode )
 	{
 		if ( pauseCode == 0 )
 		{
-			Debug.LogError( "Dissallowed pauscode: 0", this );
+			Debug.LogError( "Disallowed pauscode: 0", this );
 			return false;
 		}
 		bool returns = true; // a variable to store the result
@@ -51,7 +57,7 @@ public class World : MonoBehaviour {
 		{
 			returns = false; //mark it in the variable
 		}
-		pauseCodes[pauseCodes.Count] = pauseCode; //adds the pausecode to the end of the array
+		pauseCodes.Add( pauseCode ); //adds the pausecode to the List
 
 		return returns;
 	}
@@ -61,14 +67,20 @@ public class World : MonoBehaviour {
 	/// Forcefull pause, won't unpause without the Unpause() (forcefull unpause)
 	/// </summary>
 	/// <returns>returns true if it wasn't paused before</returns>
-	public bool Pause( ) // to pause the code, makes it not unpause unless its forced, returns true if it wasn't paused before
+	public bool Pause() // to pause the code, makes it not unpause unless its forced, returns true if it wasn't paused before
 	{
 		bool returns = true; // a variable to store the result
 		if ( isPaused ) // if its already paused
 		{
 			returns = false; //mark it in the variable
 		}
-		pauseCodes[pauseCodes.Count] = 0; //adds the pausecode for forcefull to the end of the array
+		if ( pauseCodes.Contains( 0 ) ) // if it is already forcefully paused
+		{
+			Debug.LogWarning( "Attempted to force a pause when it is already forcefully paused" ); // warn for it
+			//
+		}
+		else // we need to add it
+			pauseCodes.Add( 0 ); //adds the pausecode for forcefull to the List
 
 		return returns;
 	}
@@ -94,7 +106,7 @@ public class World : MonoBehaviour {
 	{
 		bool wasPaused = isPaused;
 		bool returns = true; // a variable to store the result
-		
+
 		if ( pauseCodes.Contains( pauseCode ) )
 		{
 			if ( allowNotThere )
@@ -105,7 +117,7 @@ public class World : MonoBehaviour {
 			return false;
 		}
 
-		
+
 
 		if ( isPaused ) // if its already paused
 		{
@@ -118,7 +130,31 @@ public class World : MonoBehaviour {
 		return returns;
 	}
 
-	void Update () {
-		
+	void Update()
+	{
+
+	}
+
+
+}
+
+/// <summary>
+/// A class to derive from if you want an autoupdated isPaused variable
+/// </summary>
+public class Pausable: MonoBehaviour
+{
+	public bool nowPaused
+	{
+		get;
+		private set;
+	}
+	internal virtual void SubscribeToPause()
+	{
+		World.current.OnPauseChange += OnPauseChange; // subsribe to the OnPauseChange event
+	}
+
+	internal virtual void OnPauseChange()
+	{
+		nowPaused = World.current.isPaused;
 	}
 }
